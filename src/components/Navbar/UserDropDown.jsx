309215f6c1cbfb1aac987ceec917d35a754780bd
wizzard95 <damian110995a@gmail.com>
2026-05-21 14:27:19 -0400

    feat: agregar informacion de usuario de sesion actual

--- src/components/Navbar/UserDropDown.jsx
+++ src/components/Navbar/UserDropDown.jsx
@@ -3,7 +3,7 @@ import { useUser } from '../../context/UserContext'
 import { logoutService } from '../../services/authServices'
 
 const UserDropDown = () => {
-    const { setUserInfo } = useUser()
+    const { userInfo, setUserInfo } = useUser()
 
     const handleLogout = async () => {
         try {
@@ -23,17 +23,26 @@ const UserDropDown = () => {
                 role="button"
                 className="btn btn-ghost btn-circle avatar"
             >
-                <div className="w-10 rounded-full">
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                        alt="Avatar"
-                    />
+                <div className="flex items-center gap-2">
+                    <span className="text-sm font-semibold hidden lg:block">
+                        {userInfo?.username}
+                    </span>
+                    <div className="w-10 rounded-full">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                            alt="Avatar"
+                        />
+                    </div>
                 </div>
             </div>
             <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow"
             >
+                <li className="menu-header px-4 py-2 text-sm font-semibold text-base-content/70 pointer-events-none">
+                    {userInfo?.username}
+                </li>
+                <div className="divider my-1"></div>
                 <li>
                     <a className="justify-between">
                         Perfil
